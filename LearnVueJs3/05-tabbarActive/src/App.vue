8ddc1f9b982970e0a2f6d00b690a8011b3741d4c
gaohanyu <2405662372@qq.com>
2022-04-25 15:26:10 +0800

    tabbar和tabbar的封装-路径问题

--- LearnVueJs3/05-tabbarActive/src/App.vue
+++ LearnVueJs3/05-tabbarActive/src/App.vue
@@ -1,42 +1,19 @@
 <template>
   <div id="app">
     <router-view/>
-    <tab-bar>
-      <tab-bar-item path="/home" activeColor="blue">
-        <img slot="item-icon" src="./assets/img/tabbar/fengjing.jpg" alt="">
-        <img slot="item-icon-active" src="./assets/img/tabbar/active.jpg" alt="">
-        <div slot="item-text">首页</div>
-      </tab-bar-item>
-      <tab-bar-item path="/category" activeColor="red">
-        <img slot="item-icon" src="./assets/img/tabbar/z.jpg" alt="">
-        <img slot="item-icon-active" src="./assets/img/tabbar/active.jpg" alt="">
-        <div slot="item-text">分类</div>
-      </tab-bar-item>
-      <tab-bar-item path="/cart" activeColor="yellow">
-        <img slot="item-icon" src="./assets/img/tabbar/fengjing.jpg" alt="">
-        <img slot="item-icon-active" src="./assets/img/tabbar/active.jpg" alt="">
-        <div slot="item-text">购物车</div>
-      </tab-bar-item>
-      <tab-bar-item path="/profile" activeColor="blue">
-        <img slot="item-icon" src="./assets/img/tabbar/z.jpg" alt="">
-        <img slot="item-icon-active" src="./assets/img/tabbar/active.jpg" alt="">
-        <div slot="item-text">我的</div>
-      </tab-bar-item>
-    </tab-bar>
+    <main-tab-bar/>
   </div>
 </template>
 
 <script>
-import TabBar from "./components/tabbar/TabBar";
-import TabBarItem from "./components/tabbar/TabBarItem";
+  import MainTabBar from "./components/MainTabBar"
 
-export default {
-  name: 'App',
-  components: {
-    TabBar,
-    TabBarItem
+  export default {
+    name: 'App',
+    components: {
+      MainTabBar
+    }
   }
-}
 </script>
 
 <style>
---
+++ LearnVueJs3/05-tabbarActive/src/components/MainTabBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <tab-bar>
+      <tab-bar-item path="/home" active-color="bule">
+        <img slot="item-icon" src="../assets/img/tabbar/fengjing.jpg" alt="">
+        <img slot="item-icon-active" src="../assets/img/tabbar/active.jpg" alt="">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category" active-color="green">
+        <img slot="item-icon" src="../assets/img/tabbar/z.jpg" alt="">
+        <img slot="item-icon-active" src="../assets/img/tabbar/active.jpg" alt="">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart" active-color="red">
+        <img slot="item-icon" src="../assets/img/tabbar/fengjing.jpg" alt="">
+        <img slot="item-icon-active" src="../assets/img/tabbar/active.jpg" alt="">
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile" active-color="yellow">
+        <img slot="item-icon" src="../assets/img/tabbar/z.jpg" alt="">
+        <img slot="item-icon-active" src="../assets/img/tabbar/active.jpg" alt="">
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+  //import TabBar from "../components/tabbar/TabBar";
+  import TabBar from "@/components/tabbar/TabBar";
+  import TabBarItem from "../components/tabbar/TabBarItem"
+
+    export default {
+      name: "MainTabBar",
+      components:{
+        TabBar,
+        TabBarItem
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
